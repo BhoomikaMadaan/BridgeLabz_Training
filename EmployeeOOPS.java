4ec03140a00cfff37a13e080639d6c3ee7dc98d8
Bhoomika Madaan <madaanbhoomika809@gmail.com>
2026-05-09 04:12:29 +0530

    Code pushed

--- EmployeeOOPS.java
+++ EmployeeOOPS.java
@@ -122,3 +122,114 @@ public class EmployeeOOPS {
     }
 
 }
+
+/*
+ * interface IDepartment {
+ * void assignDepartment(String dept);
+ * void getDepartmentDetails();
+ * }
+ * 
+ * 
+ * abstract class Employee {
+ * 
+ * private int employeeId;
+ * private String name;
+ * private double baseSalary;
+ * 
+ * Employee(int employeeId, String name, double baseSalary) {
+ * this.employeeId = employeeId;
+ * this.name = name;
+ * this.baseSalary = baseSalary;
+ * }
+ * public int getEmployeeId() {
+ * return employeeId;
+ * }
+ * 
+ * public String getName() {
+ * return name;
+ * }
+ * 
+ * public double getBaseSalary() {
+ * return baseSalary;
+ * }
+ * 
+ * 
+ * abstract double calculateSalary();
+ * 
+ * void displayDetails() {
+ * System.out.println("Employee ID: " + employeeId);
+ * System.out.println("Employee Name: " + name);
+ * }
+ * }
+ * 
+ * class FullTimeEmployee extends Employee implements IDepartment {
+ * 
+ * private String department;
+ * 
+ * FullTimeEmployee(int id, String name, double salary) {
+ * super(id, name, salary);
+ * }
+ * 
+ * @Override
+ * double calculateSalary() {
+ * return getBaseSalary();
+ * }
+ * 
+ * @Override
+ * public void assignDepartment(String dept) {
+ * department = dept;
+ * }
+ * 
+ * @Override
+ * public void getDepartmentDetails() {
+ * System.out.println("Department: " + department);
+ * }
+ * }
+ * 
+ * class PartTimeEmployee extends Employee implements IDepartment {
+ * 
+ * private int hours;
+ * private String department;
+ * 
+ * PartTimeEmployee(int id, String name, double salary, int hours) {
+ * super(id, name, salary);
+ * this.hours = hours;
+ * }
+ * 
+ * @Override
+ * double calculateSalary() {
+ * return getBaseSalary() * hours;
+ * }
+ * 
+ * @Override
+ * public void assignDepartment(String dept) {
+ * department = dept;
+ * }
+ * 
+ * @Override
+ * public void getDepartmentDetails() {
+ * System.out.println("Department: " + department);
+ * }
+ * }
+ * 
+ * 
+ * public class Main {
+ * 
+ * public static void main(String[] args) {
+ * 
+ * Employee e1 = new FullTimeEmployee(101, "Rahul", 50000);
+ * Employee e2 = new PartTimeEmployee(102, "Aman", 500, 8);
+ * 
+ * e1.assignDepartment("IT");
+ * e2.assignDepartment("HR");
+ * for (Employee e : employees) {
+ * 
+ * e.displayDetails();
+ * 
+ * System.out.println("Salary: " + e.calculateSalary());
+ * 
+ * e.getDepartmentDetails();
+ * }
+ * }
+ * }
+ */
--- Pattern.java
+++ Pattern.java
@@ -20,3 +20,32 @@ public class Pattern {
     }
 
 }
+
+3.
+
+pattern printing
+
+public class Main {
+public static void main(String[] args) {
+
+int rows = 5;
+int cols = 5;
+
+// for (int i = 1; i <= rows; i++) {
+
+// for (int j = 1; j <= cols; j++) {
+
+                
+// if (i == 1 || i == rows || j == 1 || j == cols) {
+// System.out.print("* ");
+// }
+//  else
+//  {
+//   System.out.print("  ");
+//  }
+//  }
+// System.out.println();
+        
+// }
+// }
+// }
