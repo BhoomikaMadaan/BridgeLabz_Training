8708b35c92f2f20861ce4d82cd450db35347806b
Bhoomika Madaan <madaanbhoomika809@gmail.com>
2026-05-11 12:43:11 +0530

    Added reverse string program

---
+++ reverse.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class reverse {
+
+    public static void main(String[] args) {
+        // StringBuilder sb = new StringBuilder("hello");
+        // sb.reverse();
+        // System.out.println(sb);
+
+        String s = "hello";
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+
+        }
+        System.out.println(rev);
+
+    }
+
+}
